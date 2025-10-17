ef9a283d6180a9a2381008826c4b2dfc301c56f2
aacebo <aacebowork@gmail.com>
2025-10-17 12:09:29 -0400

    Update Data.cs

--- src/OS.Agent.Storage/Models/Data.cs
+++ src/OS.Agent.Storage/Models/Data.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -8,8 +9,27 @@ namespace OS.Agent.Storage.Models;
 
 [JsonPolymorphic]
 [JsonDerivedType(typeof(Data), "data")]
-public class Data
+public class Data : IDictionary<string, JsonElement>
 {
+    [JsonIgnore]
+    public ICollection<string> Keys => Properties.Keys;
+
+    [JsonIgnore]
+    public ICollection<JsonElement> Values => Properties.Values;
+
+    [JsonIgnore]
+    public int Count => Properties.Count;
+
+    [JsonIgnore]
+    public bool IsReadOnly => Properties.IsReadOnly;
+
+    [JsonIgnore]
+    public JsonElement this[string key]
+    {
+        get => Properties[key];
+        set => Properties[key] = value;
+    }
+
     [JsonExtensionData]
     public IDictionary<string, JsonElement> Properties = new Dictionary<string, JsonElement>();
 
@@ -44,4 +64,18 @@ public class Data
 
         return data;
     }
+
+    public object? GetOrDefault(string key) => Properties.TryGetValue(key, out JsonElement value) ? value : null;
+    public T? GetOrDefault<T>(string key) => Properties.TryGetValue(key, out JsonElement value) ? value.Deserialize<T>() : default;
+    public void Add(string key, JsonElement value) => Properties.Add(key, value);
+    public bool ContainsKey(string key) => Properties.ContainsKey(key);
+    public bool Remove(string key) => Properties.Remove(key);
+    public bool TryGetValue(string key, out JsonElement value) => Properties.TryGetValue(key, out value);
+    public void Add(KeyValuePair<string, JsonElement> item) => Properties.Add(item);
+    public void Clear() => Properties.Clear();
+    public bool Contains(KeyValuePair<string, JsonElement> item) => Properties.Contains(item);
+    public void CopyTo(KeyValuePair<string, JsonElement>[] array, int arrayIndex) => Properties.CopyTo(array, arrayIndex);
+    public bool Remove(KeyValuePair<string, JsonElement> item) => Properties.Remove(item);
+    public IEnumerator<KeyValuePair<string, JsonElement>> GetEnumerator() => Properties.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
